5d059f12ad7e5104df81fc85d3ba21453828fdf0
JInsunnya <jsk5754@naver.com>
2025-02-06 23:36:20 +0900

    feat: write 페이지 추가

---
+++ src/ui/pages/write.jsx
@@ -0,0 +1,10 @@
+import { React } from "react";
+import { useNavigate } from "react-router-dom";
+import * as W from "../styles/writeStyles";
+
+const Write = () => {
+    return(
+        <div>지원서 작성 페이지</div>
+    );
+};
+export default Write;
