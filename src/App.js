e2d9ed6746853680c496bafd8e693b17e2993dc0
yujinko <rhdbwls3355@naver.com>
2025-02-08 21:03:08 +0900

    fix: 메인 페이지 로그인 후 버튼 수정

--- src/App.js
+++ src/App.js
@@ -1,8 +1,4 @@
-import {
-	BrowserRouter,
-	Route,
-	Routes,
-} from 'react-router-dom';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
 import Write from './ui/pages/write';
 import SignupPage from './ui/pages/SignupPage';
@@ -12,24 +8,21 @@ import MainPage from './ui/pages/MainPage';
 import ApplianceSubmit from './ui/pages/ApplianceSubmit';
 
 function App() {
-	return (
-		<BrowserRouter>
-			<Routes>
-				<Route path='/' element={<MainPage />} />
-				<Route path='/write' element={<Write />} />
-				<Route path='/signup' element={<SignupPage />} />
-				<Route path='/login' element={<LoginPage />} />
-				<Route
-					path='/appliancelist'
-					element={<FormListView />}
-				/>
-				<Route
-					path='/appliance/submit/:id'
-					element={<ApplianceSubmit />}
-				/>{' '}
-			</Routes>
-		</BrowserRouter>
-	);
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/write" element={<Write />} />
+        <Route path="/signup" element={<SignupPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/appliancelist" element={<FormListView />} />
+        <Route
+          path="/appliance/submit/:id"
+          element={<ApplianceSubmit />}
+        />{' '}
+      </Routes>
+    </BrowserRouter>
+  );
 }
 
 export default App;
