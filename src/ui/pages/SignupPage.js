1779f69979af75d5f7e6526c28b7c23510e1cee2
as-zini <aszini@naver.com>
2025-02-08 21:05:33 +0900

    Design:미디어 쿼리 적용

--- src/ui/pages/SignupPage.js
+++ src/ui/pages/SignupPage.js
@@ -5,6 +5,7 @@ import MarginVertical from '../components/MarginVertical'
 import dropdown_icon from '../../assets/images/dropdown_icon.png';
 import SignupDropDown from '../components/SignupDropDown';
 import { useSignup } from '../../logic/hooks/useSignup';
+import { Link } from 'react-router-dom';
 
 const getWindowDimensions = () => {
   const {innerWidth:width, innerHeight: height} = window;
@@ -56,13 +57,16 @@ const SignupPage = () => {
 
   return (
     <SignupBody>
+      <SignupInner style={{display:'flex', flexDirection:'column', justifyContent:'center', alignItems:'center'}}>
         <SignupInfoArea>
           <MarginVertical margin={124}/>
           <SignupTitle>회원가입</SignupTitle>
           <MarginVertical margin={16}/>
           <GotoLoginArea>
             <SignupText>이미 계정이 있으신가요?</SignupText>
-            <GotoLoginText>Log In</GotoLoginText>
+            <Link to={"/login"}>
+              <GotoLoginText>Log In</GotoLoginText>
+            </Link>
           </GotoLoginArea>
           <MarginVertical margin={38}/>
         </SignupInfoArea>
@@ -76,12 +80,12 @@ const SignupPage = () => {
               <SignupEl key={index}>
                 <SignupCategory>{el}</SignupCategory>
                 <MarginVertical margin={8}/>
-                <div style={{width:369, display:'flex'}}>
+                <GradeInputBody>
                   <SignupInput placeholder={`${el}을 선택해주세요`} value={grade}/>
                   <DropDownIcon onClick={() => setShowDropDown(prev => !prev)}>
                     <DropDownIconImg src={dropdown_icon}/>
                   </DropDownIcon>
-                </div>
+                </GradeInputBody>
                 <MarginVertical margin={showDropDown ? 10 : 24}/>
               </SignupEl>
               {showDropDown ?
@@ -137,7 +141,7 @@ const SignupPage = () => {
         style={{backgroundColor:!emptyArray.includes(true) && isChecked ? "" : "gray"}}
         >가입하기</SignupButton>
         <MarginVertical margin={100}/>
-
+        </SignupInner>
     </SignupBody>
   )
 }
@@ -147,7 +151,7 @@ export default SignupPage
 
 const SignupBody = styled.div`
   background-color:#F2F4F6;
-  width:100vw;
+  width:100%;
   height:100%;
   display:flex;
   flex-direction:column;
@@ -159,9 +163,24 @@ const SignupBody = styled.div`
 
 `
 
+const SignupInner = styled.div`
+  width: 90vw;
+  display:flex;
+  flex-direction:column;
+  justify-content:center;
+  align-items:center;
+  @media(max width:369px){
+    width:90vw;
+  }
+
+`
+
 const SignupInfoArea = styled.div`
   width:369px;
 
+  @media(max-width:369px){
+    width:90vw;
+  }
  
 `
 
@@ -199,10 +218,23 @@ const GotoLoginText = styled.div`
 `
 
 const SignupContentsBody = styled.div`
-  
+  display:flex;
+  justify-content:center;
+  align-items:center;
+  flex-direction:column;
+  width:369px;
+
+  @media(max-width:369px){
+    width:90vw;
+  }
 `
 
 const SignupEl = styled.div`
+  width:369px;
+
+  @media(max-width:369px){
+    width:90vw;
+  }
 
 `
 
@@ -227,13 +259,20 @@ const SignupInput = styled.input`
   font-weight:700;
   font-size:15px;
   
+  @media (max-width:380px){
+    width:90vw;
+  }
 `
 
 const SignupCheckBoxArea = styled.div`
+  width:369px;
   display:flex;
   align-items:center;
-  width:369px;
+  
   gap:5px;
+  @media(max-width:369px){
+    width:90vw;
+  }
 `
 
 const CheckBox = styled.input`
@@ -263,6 +302,10 @@ const SignupButton = styled.button`
   justify-content:center;
   align-items:center;
   box-sizing:border-box;
+
+  @media(max-width:369px){
+    width:90vw;
+  }
 `
 
 const DropDownIcon = styled.button`
@@ -279,4 +322,13 @@ const DropDownIconImg = styled.img`
 const DifferentPW = styled.div`
   color:#ED802F;
   font-weight:700;
+`
+
+const GradeInputBody = styled.div`
+  width:369px;
+  display:flex;
+
+  @media(max-width){
+    width:90vw;
+  }
 `
