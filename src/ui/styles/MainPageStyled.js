064aa9c728a605dc810a54088272a4ada87c4b9a
yujinko <rhdbwls3355@naver.com>
2025-02-08 18:18:38 +0900

    style: 메인 페이지 푸터 추가

--- src/ui/styles/MainPageStyled.js
+++ src/ui/styles/MainPageStyled.js
@@ -3,7 +3,7 @@ import backgroundImg from '../../assets/images/background_img.png';
 
 export const Body = styled.div`
   width: 100%;
-  height: 100vh;
+  height: calc(100vh - 80px);
   background-color: #212224;
   display: flex;
 
