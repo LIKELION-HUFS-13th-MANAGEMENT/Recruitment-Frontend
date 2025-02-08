c8229ad5577382666985318dfb9621d5d5ead9d5
JInsunnya <jsk5754@naver.com>
2025-02-08 23:32:30 +0900

    feat: Header 컴포넌트에 페이지 이동 기능 추가

--- src/ui/components/header.jsx
+++ src/ui/components/header.jsx
@@ -4,13 +4,27 @@ import * as H from '../styles/headerStyles';
 import Logo from '../../assets/images/logo.png';
 
 const Header = () => {
+    const navigate = useNavigate();
+    
+    const handleLogin = () => {
+        navigate(`/login`)
+    };
+
+    const handleWrite = () => {
+        navigate(`/write`)
+    };
+
+    const handleHome = () => {
+        navigate(`/`)
+    };
+
     return (
         <H.Container>
             <H.Logo src={Logo} alt="로고" />
             <H.MenuContainer>
-                <H.ButtonLogin>로그인</H.ButtonLogin>
-                <H.ButtonWrite>지원서 작성하기</H.ButtonWrite>
-                <H.ButtonHome>HOME</H.ButtonHome>
+                <H.ButtonLogin onClick={handleLogin}>로그인</H.ButtonLogin>
+                <H.ButtonWrite onClick={handleWrite}>지원서 작성하기</H.ButtonWrite>
+                <H.ButtonHome onClick={handleHome}>HOME</H.ButtonHome>
             </H.MenuContainer>
         </H.Container>
     );
